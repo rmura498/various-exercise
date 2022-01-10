160d88e1f6910d7559376c51285c6d9964455cfb
rffmu <r.ciompo@gmail.com>
2022-01-10 10:34:38 +0100

    alternate_list.py

--- alternate_list.py
+++ alternate_list.py
@@ -7,6 +7,21 @@ The object must store the first element, skip the secondo one, store the third a
 The object must respond only to methods add_item() and ad_collection()
 """
 
+"""
+If i have an object that contains behavior that i wish to reuse i can extend 
+the class using inheritance, but sometimes is better to keep the object and 
+design a class that contain this object or server and from this inner object uses only the behavior that i need
+
+"""
+
+"""
+Another situation in which composition could be better:
+if you have two classes, for example a list and a class that performs mathematical operations.
+You can inherit from both of them, but this make the design more complex, so the idea is to implement a new class 
+with two attributes, one from the math class and the other from the list class, and you can design your interface that 
+uses exactly and only the behavior that you want to use    
+"""
+
 
 class AlternateListComp():
     def __init__(self):
@@ -38,4 +53,5 @@ obj = AlternateListComp()
 obj.add_item(24)
 obj.add_collection([1, 2, 3, 4, 5, 6])
 
-print("24,2,4 ", obj.list)
+print(obj.list)
+
